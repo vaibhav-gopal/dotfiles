7488f5aed7df4d4f473991e6f9b43b82cd37a6fa
Vaibhav Gopal <vabsgop@gmail.com>
2025-10-13 17:44:10 -0400

    minor fix

--- home/common/vim.nix
+++ home/common/vim.nix
@@ -8,7 +8,7 @@
     enable = true;
   };
 
-  home.file."${config.home.homeDirectory}/.config/nvim/init.lua".source = hmPaths.homeCmmonConfigsDir + "/vim.d/init.lua";
+  home.file."${config.home.homeDirectory}/.config/nvim/init.lua".source = hmPaths.homeCommonConfigsDir + "/vim.d/init.lua";
   home.file."${config.home.homeDirectory}/.config/nvim/init.vim".source = hmPaths.homeCommonConfigsDir + "/vim.d/init.vim";
   home.file."${config.home.homeDirectory}/.vimrc".source = hmPaths.homeCommonConfigsDir + "/vim.d/vimrc";
 }
