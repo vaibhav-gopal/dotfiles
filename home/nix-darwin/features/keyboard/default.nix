0092a1447bfcf15ab4777d63fda0b3ff1512f522
Vaibhav Gopal <vabsgop@gmail.com>
2025-11-29 20:43:42 -0500

    doesn't work at all ; fix later

--- home/nix-darwin/features/keyboard/default.nix
+++ home/nix-darwin/features/keyboard/default.nix
@@ -3,8 +3,8 @@
 let
   cfg = config.darwin.features.keyboard;
   bundlePath = ./VaibhavMacKeyboardLayouts.bundle;
-  layoutsPath = "${config.home.homeDirectory}/Library/Keyboard\ Layouts/VaibhavMacKeyboardLayouts.bundle";
-  layoutsPath_Raw = "$HOME/Library/Keyboard\ Layouts/";
+  layoutsPath = "${config.home.homeDirectory}/Library/Keyboard\ Layouts/";
+  layoutsDirPath = "${config.home.homeDirectory}/Library/Keyboard\ Layouts/";
 in {
   options.darwin.features.keyboard = {
     enable = lib.mkEnableOption "Enable darwin keyboard";
@@ -17,20 +17,13 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    # For symlink (doesn't work)
-    # home.file.${layoutsPath}.source = cfg.bundlePath;
-
-    # For copying the file
-    system.activationScripts.copyMyFiles = {
-    deps = [ "users" ]; 
-    text = ''
+  config.home.activation.copyKeyboardLayoutBundle = lib.mkIf cfg.enable ( 
+    lib.hm.dag.entryAfter ["writeBoundary"] ''
       # Create the target directory if it doesn't exist
-      mkdir -p ${layoutsPath_Raw}
+      mkdir -p ${layoutsDirPath}
 
       # Copy files from a source in your Nix store to the destination
-      cp -rf ${bundlePath} /path/to/destination/directory/
-    '';
-  };
-  };
+      cp -rf ${bundlePath} ${layoutsPath}
+    ''
+  );
 }
