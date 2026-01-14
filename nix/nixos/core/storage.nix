7ecd26e94bcb735f2e315692c7bfb06dbfccb810
Vaibhav Gopal <vabsgop@gmail.com>
2026-01-13 23:56:42 -0500

    adding formatting / partitioning utilities

---
+++ nix/nixos/core/storage.nix
@@ -0,0 +1,19 @@
+{ config, lib, usrlib, pkgs, ... }:
+let
+  cfg = config.core.storage;
+in {
+  options.core.storage = {
+    enable = usrlib.mkEnableOptionTrue "enable core storage configuration packages";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment = {
+      systemPackages = [
+        pkgs.exfatprogs # provides: mkfs.exfat, ...
+        pkgs.parted # provides gparted GUI utility
+        pkgs.gdisk # provides: gdisk (GPT partition tables)
+        # pkgs.fdisk # provides: fdisk (MBR partition tables, provided by nix by default)
+      ];
+    };
+  };
+}
